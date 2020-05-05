aa77e96179e547be49d2fc88dd57a5c232890dd8
qaconcept <64862177+qaconcept@users.noreply.github.com>
2020-05-05 14:56:48 -0400

    Create test_fib.py
    This test submits 'http://localhost:5000/?n=5' and validates response = [0, 1, 1, 2, 3]

---
+++ test_fib.py
@@ -0,0 +1,7 @@
+import requests
+
+res = requests.get('http://localhost:5000/?n=5')
+if (res.text == '[0, 1, 1, 2, 3]'):
+    print("Passed, 1st 5 numbers in the fibonacci sequence is "+res.text)
+else:
+    print("Failed "+res.text)
