b47dd4adceb26eca2f18096fd986adc1d996b4de
qaconcept <64862177+qaconcept@users.noreply.github.com>
2020-05-05 14:07:59 -0400

    Create fib_api.py

---
+++ fib_api.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+#Fibonacci function
+def fibo(n: int):
+    l=[]
+    a = 0
+    b = 1
+    """
+    Calculating the fibonacci numbers
+    """
+    if n < 1:
+        return 'Only numbers > 0 are accepted'
+    elif n == 1:
+        return a
+    else:
+        l.append(a)
+        l.append(b)
+
+        for i in range (2, n):
+            c = a + b
+            a = b
+            b = c
+            l.append(c)
+        return(l)
+
+#route
+@app.route("/")
+def send_fibo():
+    """
+    Sending the fibonacci list to the user or if
+    he uses improper input
+    """
+    try:
+        return str(fibo(int(request.args['n'])))
+    except ValueError:
+        return "Please use a number"
+
+if __name__ == '__main__':
+    app.run()
